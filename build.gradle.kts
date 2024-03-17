97cce8aec1a2231b5fc655ad2cfbc0cd62f4ef4b
arsinde <arsinde@gmail.com>
2024-03-17 21:06:56 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    application
+}
+
+group = "ru.otus"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
